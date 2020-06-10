b5fea2daf03d4584f8325064a5f8b6020755e140
Spencer Will <spencer.a.will@gmail.com>
2020-06-10 00:19:48 -0400

    Added a static Fill method with way too many checks. TODO: possibly move some parameters to a separate class.
    Added a Sub method which will eventually create a new array from a region defined in an existing array.

--- NDimArray/NDimArray/NDimArray.cs
+++ NDimArray/NDimArray/NDimArray.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace NDimArray
@@ -170,6 +171,76 @@ namespace NDimArray
         {
             return SpatialEnumerator<T>.GetStandardPriorityList(Rank);
         }
+        #endregion
+
+        #region Statics
+
+        public static void Fill(NDimArray<T> array, int[] start, int[] end, int[] dimPriorities, Func<int[], T, T> fillRule)
+        {
+            //THERE ARE WAY TOO MANY CONDITIONS HERE
+            //perhaps move parameters: start, end, dimPriorities to a separate class/struct which does the checks (SRP)
+
+            // null checks
+            if (array == null)
+                throw new ArgumentNullException("array", "array is null");
+            if (start == null)
+                throw new ArgumentNullException("start", "start is null");
+            if (end == null)
+                throw new ArgumentNullException("end", "end is null");
+            if (array == null)
+                throw new ArgumentNullException("dimPriorities", "dimPriorities is null");
+
+            //no item checks
+            if (start.Length < 1)
+                throw new ArgumentException("start", "start must have at least one element");
+            if (end.Length < 1)
+                throw new ArgumentException("end", "end must have at least one element");
+            if (dimPriorities.Length < 1)
+                throw new ArgumentException("dimPriorities", "dimPriorities must have at least one element");
+
+            //length checks
+            if (start.Length != array.Rank)
+                throw new ArgumentException("start", "start must have the same number of indices as the rank of the array");
+            if (end.Length != array.Rank)
+                throw new ArgumentException("end", "end must have the same number of indices as the rank of the array");
+            if (dimPriorities.Length != array.Rank)
+                throw new ArgumentException("dimPriorities", "dimPriorities must have the same number of indices as the rank of the array");
+
+            if (!array.ValidIndices(start))
+                throw new IndexOutOfRangeException("1 or more dimension indices in start was out of the range of the array");
+            if (!array.ValidIndices(end))
+                throw new IndexOutOfRangeException("1 or more dimension indices in end was out of the range of the array");
+            if (!dimPriorities.SequenceEqual(dimPriorities.Distinct()))
+                throw new ArgumentException("dimPriorities", "dimPriorities must be distinct (every element must be unique)");
+            if (!Array.TrueForAll(dimPriorities, x => x >= 0 && x <= array.Rank - 1))
+                throw new ArgumentOutOfRangeException("dimPriorities", "one or more elements of dimPriorities was an invalid dimension");
+
+            array.Enumerate(start, end, dimPriorities, (index, item) => array[index] = fillRule(index, item));
+        }
+
+        public static void Fill(NDimArray<T> array, int[] start, int[] end, int[] dimPriorities, T value) =>
+            Fill(array, start, end, dimPriorities, (index, item) => value);
+
+        public static void Fill(NDimArray<T> array, int[] start, int[] end, Func<int[], T, T> fillRule) =>
+            Fill(array, start, end, array.GetStandardEnumerationPriorities(), fillRule);
+
+        public static void Fill(NDimArray<T> array, int[] start, int[] end, T value) =>
+            Fill(array, start, end, (index, item) => value);
+
+        public static void Fill(NDimArray<T> array, Func<int[], T, T> fillRule) =>
+            Fill(
+                array,
+                SpatialEnumerator<T>.LowerBoundaries(array.array),
+                SpatialEnumerator<T>.UpperBoundaries(array.array),
+                array.GetStandardEnumerationPriorities(),
+                fillRule);
+
+        public static void Fill(NDimArray<T> array, T value) =>
+            Fill(array, (index, item) => value);
+
+        public static NDimArray<T> Sub(NDimArray<T> array, int[] start, int[] end, bool preserveIndices = false) =>
+            throw new NotImplementedException();
+
         #endregion
     }
 }
