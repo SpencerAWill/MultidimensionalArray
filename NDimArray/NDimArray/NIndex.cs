afdf6a91088afaa32fedea2a557fa225a3e6ddc3
Spencer Will <spencer.a.will@gmail.com>
2020-06-14 23:20:35 -0400

    Add Origin(int dimension) method to NIndex. Add Unit(INIndex index) method to NIndex. Implement IEnumerable<int> Property fixes.

--- NDimArray/NDimArray/NIndex.cs
+++ NDimArray/NDimArray/NIndex.cs
@@ -1,19 +1,21 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace NDimArray
 {
-    public class NIndex : INIndex
+    public class NIndex : INIndex, IEnumerable<int>
     {
         private int[] _indices;
-
-        public IReadOnlyList<int> Indices { get => _indices; }
+        public int[] Indices { get => _indices; }
+        IReadOnlyList<int> INIndex.Indices { get => _indices; }
 
         public int this[int dimension]
         {
-            get => Indices[dimension];
-            private set => _indices[dimension] = value;
+            get => _indices[dimension];
+            set => _indices[dimension] = value;
         }
 
         public NIndex(int[] indices)
@@ -45,10 +47,58 @@ namespace NDimArray
             }
             return diff;
         }
+
+        public static int[] Unit(IEnumerable<int> index)
+        {
+            if (index == null)
+                throw new ArgumentNullException("index", "index is null");
+            var count = index.Count();
+            if (count == 0)
+                throw new ArgumentException("index", "index cannot be empty");
+
+            var retIndex = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                var item = index.ElementAt(i);
+                retIndex[i] = item == 0 ? 0 : item / Math.Abs(item);
+            }
+
+            return retIndex;
+        }
+
+        public static int[] Unit(INIndex index)
+        {
+            if (index == null)
+                throw new ArgumentNullException("index", "index is null");
+
+            return Unit(index.Indices);
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            foreach (var item in _indices)
+                yield return item;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() =>
+            GetEnumerator();
+
+        public NIndex Origin() => 
+            Origin(Indices.Length);
+
+        public static NIndex Origin(int dimensions)
+        {
+            if (dimensions < 1)
+                throw new ArgumentOutOfRangeException("dimensions", "dimensions must be greater than 0");
+
+            return new NIndex((int[])Array.CreateInstance(typeof(int), dimensions));
+        }
     }
 
-    public interface INIndex
+    public interface INIndex : IEnumerable<int>
     {
         IReadOnlyList<int> Indices { get; }
+        int this[int dimension] { get; }
     }
 }
